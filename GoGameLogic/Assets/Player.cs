23b8584ce967102a60fa8644f0a4ace8154c274b
Festermo <79100655+Festermo@users.noreply.github.com>
2021-02-19 19:49:50 +0300

    Работает передвижение по линиям

---
+++ GoGameLogic/Assets/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public GameObject NodeHandler;
+	public GameObject VerLineHandler;
+	public GameObject HorLineHandler;
+	private Node NodeFuncs;
+	private VerticalLine VerLineFuncs;
+	private HorizontalLine HorLineFuncs;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		NodeFuncs = NodeHandler.GetComponent<Node>();
+		VerLineFuncs = VerLineHandler.GetComponent<VerticalLine>();
+		HorLineFuncs = HorLineHandler.GetComponent<HorizontalLine>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown("a"))
+		{
+			if (NodeFuncs.CheckIfNodeExist(transform.position, 'x', -1)
+			&& HorLineFuncs.CheckIfThereIsLine(transform.position, -1, transform.position + new Vector3(-1, 0, 0)))
+			{
+				transform.position += new Vector3(-1, 0, 0);
+			}
+		}
+		if (Input.GetKeyDown("d"))
+		{
+			if (NodeFuncs.CheckIfNodeExist(transform.position, 'x', 1)
+			&& HorLineFuncs.CheckIfThereIsLine(transform.position, 1, transform.position + new Vector3(1,0,0)))
+			{
+				transform.position += new Vector3(1, 0, 0);
+			}
+		}
+		if (Input.GetKeyDown("w"))
+		{
+			if (NodeFuncs.CheckIfNodeExist(transform.position, 'y', 1) 
+			&& VerLineFuncs.CheckIfThereIsLine(transform.position, 1, transform.position + new Vector3(0, 0, 1)))
+			{
+				transform.position += new Vector3(0, 0, 1);
+			}
+		}
+		if (Input.GetKeyDown("s"))
+		{
+			if (NodeFuncs.CheckIfNodeExist(transform.position, 'y', -1) && VerLineFuncs.CheckIfThereIsLine(transform.position, -1, transform.position + new Vector3(0, 0, -1)))
+			{
+				transform.position += new Vector3(0, 0, -1);
+			}
+		}
+
+	}
+}
