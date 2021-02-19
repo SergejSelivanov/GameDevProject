66067bc6e93b0d42ab5987bef983f45d3b87ecfd
Festermo <79100655+Festermo@users.noreply.github.com>
2021-02-19 20:34:38 +0300

    Уничтожение врагов, если заходишь не с лица

---
+++ GoGameLogic/Assets/Scripts/MotionlessEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionlessEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool CheckIfFacing(GameObject player)
+    {
+        if (gameObject.transform.rotation.eulerAngles.y == 0 
+        && player.transform.rotation.eulerAngles.y == gameObject.transform.rotation.y + 180)
+            return true;
+        if (gameObject.transform.rotation.eulerAngles.y == 90 
+        && player.transform.rotation.eulerAngles.y - 180 == gameObject.transform.rotation.eulerAngles.y)
+            return true;
+        if (gameObject.transform.rotation.eulerAngles.y == 180 
+        && player.transform.rotation.eulerAngles.y + 180 == gameObject.transform.rotation.eulerAngles.y)
+            return true;
+        if (gameObject.transform.rotation.eulerAngles.y == 270 
+        && player.transform.rotation.eulerAngles.y == gameObject.transform.rotation.eulerAngles.y - 180)
+            return true;
+        return false;
+    }
+
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Transform PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (transform.position.x == player.transform.position.x
+        && transform.position.z == player.transform.position.z
+        && !CheckIfFacing(player))
+            Destroy(gameObject);
+    }
+}
