ec12fdd9a62c96e665824a34619d330d0d086a56
Festermo <79100655+Festermo@users.noreply.github.com>
2021-02-20 15:58:01 +0300

    Плавное движение персонажа

--- GoGameLogic/Assets/Scripts/Player.cs
+++ GoGameLogic/Assets/Scripts/Player.cs
@@ -11,6 +11,51 @@ public class Player : MonoBehaviour
 	private VerticalLine VerLineFuncs;
 	private HorizontalLine HorLineFuncs;
 
+	IEnumerator WalkLeft()
+	{
+		for (float i = 0; i < 1; i += 0.2f)
+		{
+			transform.position += new Vector3(-0.2f, 0, 0);
+			yield return new WaitForSeconds(0.1f);
+		}
+		transform.position = new Vector3(Mathf.Round(transform.position.x), transform.position.y, transform.position.z);
+		yield return null;
+	}
+
+	IEnumerator WalkRight()
+    {
+        for (float i = 0;  i < 1; i += 0.2f)
+        {
+			transform.position += new Vector3(0.2f, 0, 0);
+			yield return new WaitForSeconds(0.1f);
+        }
+		transform.position = new Vector3(Mathf.Round(transform.position.x), transform.position.y, transform.position.z);
+		yield return null;
+    }
+
+	IEnumerator WalkUp()
+	{
+		for (float i = 0; i < 1; i += 0.2f)
+		{
+			transform.position += new Vector3(0, 0, 0.2f);
+			yield return new WaitForSeconds(0.1f);
+		}
+		transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Round(transform.position.z));
+		yield return null;
+	}
+
+	IEnumerator WalkDown()
+	{
+		for (float i = 0; i < 1; i += 0.2f)
+		{
+			transform.position += new Vector3(0, 0, -0.2f);
+			yield return new WaitForSeconds(0.1f);
+		}
+		transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Round(transform.position.z));
+		Debug.Log(transform.position);
+		yield return null;
+	}
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -24,11 +69,14 @@ public class Player : MonoBehaviour
 	{
 		if (Input.GetKeyDown("a"))
 		{
+			//transform.position = FinalPos;
+			//Debug.Log(transform.position);
 			if (NodeFuncs.CheckIfNodeExist(transform.position, 'x', -1)
 			&& HorLineFuncs.CheckIfThereIsLine(transform.position, -1, transform.position + new Vector3(-1, 0, 0)))
 			{
 				transform.rotation = Quaternion.Euler(0, 270, 0);
-				transform.position += new Vector3(-1, 0, 0);
+				StartCoroutine("WalkLeft");
+				//transform.position += new Vector3(-1, 0, 0);
 			}
 		}
 		if (Input.GetKeyDown("d"))
@@ -37,7 +85,12 @@ public class Player : MonoBehaviour
 			&& HorLineFuncs.CheckIfThereIsLine(transform.position, 1, transform.position + new Vector3(1,0,0)))
 			{
 				transform.rotation = Quaternion.Euler(0, 90, 0);
-				transform.position += new Vector3(1, 0, 0);
+				//FinalPos = transform.position + new Vector3(1, 0, 0);
+				StartCoroutine("WalkRight");
+				//StopCoroutine("Walk");
+				//Debug.Log(transform.position);
+				//Walk();
+				//transform.position += new Vector3(1, 0, 0);
 			}
 		}
 		if (Input.GetKeyDown("w"))
@@ -46,7 +99,8 @@ public class Player : MonoBehaviour
 			&& VerLineFuncs.CheckIfThereIsLine(transform.position, 1, transform.position + new Vector3(0, 0, 1)))
 			{
 				transform.rotation = Quaternion.Euler(0, 0, 0);
-				transform.position += new Vector3(0, 0, 1);
+				StartCoroutine("WalkUp");
+				//transform.position += new Vector3(0, 0, 1);
 			}
 		}
 		if (Input.GetKeyDown("s"))
@@ -54,7 +108,8 @@ public class Player : MonoBehaviour
 			if (NodeFuncs.CheckIfNodeExist(transform.position, 'y', -1) && VerLineFuncs.CheckIfThereIsLine(transform.position, -1, transform.position + new Vector3(0, 0, -1)))
 			{
 				transform.rotation = Quaternion.Euler(0, 180, 0);
-				transform.position += new Vector3(0, 0, -1);
+				StartCoroutine("WalkDown");
+				//transform.position += new Vector3(0, 0, -1);
 			}
 		}
 
