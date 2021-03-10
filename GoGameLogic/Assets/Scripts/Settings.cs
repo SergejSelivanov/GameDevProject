eab767183b39b7f617fcef43acd6a874777644aa
Festermo <79100655+Festermo@users.noreply.github.com>
2021-03-10 14:34:39 +0300

    Добавлено меню и музыка(есть ползунок для громкости в настройках)

---
+++ GoGameLogic/Assets/Scripts/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject SettingsPanel;
+    public GameObject MenuPanel;
+
+    public void OpenSettings()
+    {
+        MenuPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        MenuPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
