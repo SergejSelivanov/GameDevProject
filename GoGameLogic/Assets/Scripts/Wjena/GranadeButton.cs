3cce0c934365a65f7025d71621fe2126335cb5c4
SergejSelivanov <59225535+SergejSelivanov@users.noreply.github.com>
2021-03-19 20:14:36 +0300

    Полностью рабочая приманка

--- GoGameLogic/Assets/Scripts/Wjena/GranadeButton.cs
+++ GoGameLogic/Assets/Scripts/Wjena/GranadeButton.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class GranadeButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    // public GameObject NodeHandler;
-    // private Node NodeFuncs;
     public GameObject PlayerHandler;
     private Player PlayerFuncs;
     public void ft_pressed_granade_button()
     {
-        //flagGranade = false;
         if (PlayerFuncs.IsflagGranade == true)
         {
             Time.timeScale = 0;
@@ -23,13 +19,11 @@ public class GranadeButton : MonoBehaviour
             Time.timeScale = 1;
             PlayerFuncs.IsflagGranade = true;
         }
-        //Debug.Log(flagGranade);
     }
 
     void Start()
     {
         PlayerFuncs = PlayerHandler.GetComponent<Player>();
-        // NodeFuncs = NodeHandler.GetComponent<Node>();
     }
 
     // Update is called once per frame
