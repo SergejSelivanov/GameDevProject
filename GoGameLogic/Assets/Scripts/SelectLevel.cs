7b967fec17fb9d36002d5fa021059cf774d6c498
Festermo <79100655+Festermo@users.noreply.github.com>
2021-05-21 16:10:51 +0300

    LevelSelector

---
+++ GoGameLogic/Assets/Scripts/SelectLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+    public void Select(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+}
