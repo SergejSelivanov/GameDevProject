e11be550cc8ec727f4c2cedd59ffda2ffc22ef68
Festermo <79100655+Festermo@users.noreply.github.com>
2021-06-22 18:03:45 +0300

    Arrows to move player

--- GoGameLogic/Assets/Scripts/Player.cs
+++ GoGameLogic/Assets/Scripts/Player.cs
@@ -1427,6 +1427,119 @@ public bool IsThereGate(Transform ObjCoord)
 		yield return null;
 	}
 
+    public void Up()
+    {
+		Quaternion OldRotation;
+		if (IsMovable == true && IsWaiting == false && Time.timeScale == 1 && ProjectionIsActive == false)
+		{
+			if (NodeFuncs.CheckIfNodeExist(transform.position, 'y', 1)
+		&& VerLineFuncs.CheckIfThereIsLine(transform.position, 1, transform.position + new Vector3(0, 0, 1)))
+			{
+				OldRotation = transform.rotation;
+				transform.rotation = Quaternion.Euler(0, 0, 0);
+				if (!IsThereGate(transform) && !IsThereCamera(transform) && (CheckIfThereIsMotEnemy(gameObject) || CheckIfThereIsMovingEnemy(gameObject)))
+				{
+					transform.rotation = OldRotation;
+					IsWaiting = true;
+					StartCoroutine("RotateAndKill", 0);
+				}
+				else if (!IsThereGate(transform) && !IsThereCamera(transform))
+				{
+					transform.rotation = OldRotation;
+					IsWaiting = true;
+					StartCoroutine("Rotate", 0);
+				}
+				else
+					transform.rotation = OldRotation;
+			}
+		}
+	}
+
+    public void Down()
+    {
+		Quaternion OldRotation;
+		if (IsMovable == true && IsWaiting == false && Time.timeScale == 1 && ProjectionIsActive == false)
+		{
+			if (NodeFuncs.CheckIfNodeExist(transform.position, 'y', -1)
+				 && VerLineFuncs.CheckIfThereIsLine(transform.position, -1, transform.position + new Vector3(0, 0, -1)))
+			{
+				OldRotation = transform.rotation;
+				transform.rotation = Quaternion.Euler(0, 180, 0);
+				if (!IsThereGate(transform) && !IsThereCamera(transform) && (CheckIfThereIsMotEnemy(gameObject) || CheckIfThereIsMovingEnemy(gameObject)))
+				{
+					transform.rotation = OldRotation;
+					IsWaiting = true;
+					StartCoroutine("RotateAndKill", 180);
+				}
+				else if (!IsThereGate(transform) && !IsThereCamera(transform))
+				{
+					transform.rotation = OldRotation;
+					IsWaiting = true;
+					StartCoroutine("Rotate", 180);
+
+				}
+				else
+					transform.rotation = OldRotation;
+			}
+		}
+	}
+
+	public void Right()
+    {
+		Quaternion OldRotation;
+		if (IsMovable == true && IsWaiting == false && Time.timeScale == 1 && ProjectionIsActive == false)
+		{
+			if (NodeFuncs.CheckIfNodeExist(transform.position, 'x', 1)
+		&& HorLineFuncs.CheckIfThereIsLine(transform.position, 1, transform.position + new Vector3(1, 0, 0)))
+			{
+				OldRotation = transform.rotation;
+				transform.rotation = Quaternion.Euler(0, 90, 0);
+				if (!IsThereGate(transform) && !IsThereCamera(transform) && (CheckIfThereIsMotEnemy(gameObject) || CheckIfThereIsMovingEnemy(gameObject)))
+				{
+					transform.rotation = OldRotation;
+					IsWaiting = true;
+					StartCoroutine("RotateAndKill", 90);
+				}
+				else if (!IsThereGate(transform) && !IsThereCamera(transform))
+				{
+					transform.rotation = OldRotation;
+					IsWaiting = true;
+					StartCoroutine("Rotate", 90);
+				}
+				else
+					transform.rotation = OldRotation;
+			}
+		}
+	}
+
+	public void Left()
+    {
+		Quaternion OldRotation;
+		if (IsMovable == true && IsWaiting == false && Time.timeScale == 1 && ProjectionIsActive == false)
+		{
+			if (NodeFuncs.CheckIfNodeExist(transform.position, 'x', -1)
+		&& HorLineFuncs.CheckIfThereIsLine(transform.position, -1, transform.position + new Vector3(-1, 0, 0)))
+			{
+				OldRotation = transform.rotation;
+				transform.rotation = Quaternion.Euler(0, 270, 0);
+				if (!IsThereGate(transform) && !IsThereCamera(transform) && (CheckIfThereIsMotEnemy(gameObject) || CheckIfThereIsMovingEnemy(gameObject)))
+				{
+					transform.rotation = OldRotation;
+					IsWaiting = true;
+					StartCoroutine("RotateAndKill", 270);
+				}
+				else if (!IsThereGate(transform) && !IsThereCamera(transform))
+				{
+					transform.rotation = OldRotation;
+					IsWaiting = true;
+					StartCoroutine("Rotate", 270);
+				}
+				else
+					transform.rotation = OldRotation;
+			}
+		}
+	}
+
 	// Start is called before the first frame update
 	void Start()
 	{
---
+++ GoGameLogic/Assets/Scripts/RotateArrows.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateArrows : MonoBehaviour
+{
+    public GameObject Arrows;
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Arrows.transform.rotation = Quaternion.Euler(0, 0, mainCamera.transform.rotation.eulerAngles.y);
+    }
+}
