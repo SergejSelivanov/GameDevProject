1044b6808c43a30838bc327b4d0d753c5029b2b6
Festermo <79100655+Festermo@users.noreply.github.com>
2021-06-25 22:00:03 +0300

    Continue

--- GoGameLogic/Assets/Scripts/MotionlessEnemy.cs
+++ GoGameLogic/Assets/Scripts/MotionlessEnemy.cs
@@ -18,7 +18,6 @@ public class MotionlessEnemy : MonoBehaviour
     {
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
 		int RequiredAngle = 0;
-
 		if (player.transform.position.x > gameObject.transform.position.x)
 			RequiredAngle = 270;
 		else if (player.transform.position.x < gameObject.transform.position.x)
@@ -27,189 +26,21 @@ public class MotionlessEnemy : MonoBehaviour
 			RequiredAngle = 180;
 		else
 			RequiredAngle = 0;
-
-
-		//int RequiredAngle = LineMovingEnemyFuncs.Opposite(ObjectToRotate);
 		int playerangle = (int)player.transform.rotation.eulerAngles.y;
-		//string EnemyTag = "MotionlessEnemy";
-		if (playerangle == 0)
+		int Diff = RequiredAngle - playerangle;
+		if (Diff == 270 || Diff == -270)
+			Diff = -Diff % 180;
+		if (Diff != 0)
 		{
-			if (RequiredAngle == 0)
+			player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 1);
+			for (int i = 0; i < 30; i++)
 			{
-				yield return null;
+				gameObject.transform.rotation = Quaternion.Euler(0, (int)gameObject.transform.rotation.eulerAngles.y + Diff / 30, 0);
+				yield return new WaitForSeconds(0.0133f);
 			}
-			if (RequiredAngle == 90)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 1);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y + 3, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 90, 0);
-				yield return null;
-			}
-			if (RequiredAngle == 180)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 3);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y + 6, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 180, 0);
-				yield return null;
-			}
-			if (RequiredAngle == 270)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 2);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y - 3, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 2);
-				player.transform.rotation = Quaternion.Euler(0, 270, 0);
-				yield return null;
-			}
+			player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
+			gameObject.transform.rotation = Quaternion.Euler(0, RequiredAngle, 0);
 		}
-		if (playerangle == 90)
-		{
-			if (RequiredAngle == 90)
-			{
-				yield return null;
-			}
-			if (RequiredAngle == 0)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 2);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y - 3, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 0, 0);
-				yield return null;
-			}
-			if (RequiredAngle == 180)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 1);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y + 3, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 180, 0);
-				yield return null;
-			}
-			if (RequiredAngle == 270)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 3);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y - 6, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 270, 0);
-				yield return null;
-			}
-		}
-		if (playerangle == 180)
-		{
-			if (RequiredAngle == 180)
-			{
-				yield return null;
-			}
-			if (RequiredAngle == 0)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 3);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y + 6, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 0, 0);
-				yield return null;
-			}
-			if (RequiredAngle == 90)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 2);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y - 3, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 90, 0);
-				yield return null;
-			}
-			if (RequiredAngle == 270)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 1);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y + 3, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 270, 0);
-				yield return null;
-			}
-		}
-		if (playerangle == 270)
-		{
-			if (RequiredAngle == 270)
-			{
-				yield return null;
-			}
-			if (RequiredAngle == 0)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 1);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y + 3, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 0, 0);
-				yield return null;
-			}
-			if (RequiredAngle == 90)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 3);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y - 6, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 90, 0);
-				yield return null;
-			}
-			if (RequiredAngle == 180)
-			{
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 2);
-				for (int i = 0; i < 30; i++)
-				{
-					player.transform.rotation = Quaternion.Euler(0, (int)player.transform.rotation.eulerAngles.y - 3, 0);
-					yield return new WaitForSeconds(0.0133f);
-				}
-				player.GetComponentInChildren<Animator>().SetInteger("IsRotating", 0);
-				player.transform.rotation = Quaternion.Euler(0, 180, 0);
-				yield return null;
-			}
-		}
-		//yield return null;
-
-
-
-
-
 		yield return new WaitForSeconds(1);
 		for (int i = 0; i < PlayerFuncs.EnemiesKill.Length; i++)
 		{
@@ -219,8 +50,7 @@ public class MotionlessEnemy : MonoBehaviour
 				break;
 			}
 		}
-		gameObject.GetComponent<Animator>().SetBool("IsDead", true);
-		//yield return new WaitForSeconds(3);
+		gameObject.transform.GetChild(0).GetComponent<Animator>().SetBool("IsDead", true);
 		yield return new WaitForSeconds(0.5f);
 		Time.timeScale = 0.6f;
 		PlayerHandler.GetComponent<Animator>().SetBool("IsTaunting", false);
@@ -231,7 +61,6 @@ public class MotionlessEnemy : MonoBehaviour
 
     private void OnMouseDown()
     {
-		//Debug.Log("REGISTERED");
 		if (Time.timeScale == 1)
 		{
 			GameObject player = GameObject.FindGameObjectWithTag("Player");
@@ -243,23 +72,10 @@ public class MotionlessEnemy : MonoBehaviour
 				{
 					Time.timeScale = 0.99f;
 					PlayerFuncs.gameObject.GetComponent<Animator>().SetBool("IsTaunting", true);
-                    /*for (int i = 0; i < PlayerFuncs.EnemiesKill.Length; i++)
-                    {
-						if (PlayerFuncs.EnemiesKill[i] == null)
-                        {
-							PlayerFuncs.EnemiesKill[i] = gameObject;
-							break;
-                        }
-                    }
-					gameObject.GetComponent<Animator>().SetBool("IsDead", true);*/
 					StartCoroutine("StopBreaking");
-					//Debug.Log(PlayerFuncs.EnemiesKill);
-					//Destroy(gameObject);
 					if (GameObject.FindObjectOfType<FillKnife>() != null)
 						GameObject.FindObjectOfType<FillKnife>().GetComponent<Image>().fillAmount = 0; 
-					//PlayerFuncs.SkillSetter = 0;
 					PlayerFuncs.IsPlayerMovable = true;
-					//StartCoroutine("StopBreaking");
 				}
 				PlayerFuncs.KnifeReady = false;
 			}
@@ -300,22 +116,8 @@ public class MotionlessEnemy : MonoBehaviour
 		return false;
 	}
 
-
 	public bool CheckifPlayerInfrontofEnemy(GameObject player, GameObject Enemy)
 	{
-		/*Debug.Log("ENemy:");
-		Debug.Log(Enemy);
-		Debug.Log(Enemy.transform.position.z);
-		Debug.Log("player:");
-		Debug.Log(player.transform.position.z);*/
-		/*if (Enemy.transform.rotation.eulerAngles.y == 90)
-		{
-			//Debug.Log(Enemy.transform.rotation.eulerAngles.y == 90);
-			Debug.Log(Enemy.transform.position.z == player.transform.position.z);
-			Debug.Log(Enemy.transform.position.x + 1 == player.transform.position.x);
-			Debug.Log(HorLineFuncs.CheckIfThereIsLine(Enemy.transform.position, 1, Enemy.transform.position + new Vector3(1, 0, 0)));
-		}*/
-		//Debug.Log(player.transform.position);
 		if (Enemy.transform.rotation.eulerAngles.y == 0
 		&& Enemy.transform.position.x == player.transform.position.x
 		&& Enemy.transform.position.z + 1 == player.transform.position.z
@@ -341,12 +143,6 @@ public class MotionlessEnemy : MonoBehaviour
 
 	public bool CheckifPlayerInfrontofEnemy(GameObject player)
 	{
-		/*Debug.Log(gameObject);
-		//Debug.Log("HERE");
-		Debug.Log(gameObject.transform.rotation.eulerAngles.y == 0);
-		Debug.Log(gameObject.transform.position.x == player.transform.position.x);
-		Debug.Log(gameObject.transform.position.z + 1 == player.transform.position.z);
-		Debug.Log(VerLineFuncs.CheckIfThereIsLine(gameObject.transform.position, 1, gameObject.transform.position + new Vector3(0, 0, 1)));*/
 		if (gameObject.transform.rotation.eulerAngles.y == 0 
 		&& gameObject.transform.position.x == player.transform.position.x 
 		&& gameObject.transform.position.z + 1 == player.transform.position.z
@@ -378,53 +174,19 @@ public class MotionlessEnemy : MonoBehaviour
 		KnifeFuncs = KnifeHandler.GetComponent<ThrowKnife>();
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-		//Transform PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-		//Debug.Log(PlayerFuncs.Invisible);
 		GameObject projection = GameObject.FindGameObjectWithTag("Projection");
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
-		//Debug.Log(PlayerFuncs.LightOffTurns >= 0);
-		//Debug.Log(PlayerFuncs.LightOffTurns);
 		if (transform.position.x == player.transform.position.x
 		&& transform.position.z == player.transform.position.z
 		&& (!CheckIfFacing(player) || PlayerFuncs.Invisible >= 0 || PlayerFuncs.LightOffTurns >= 0))
 		{
-			//Destroy(gameObject);
-			/*gameObject.GetComponent<Animator>().SetBool("IsDead", true);
-			for (int j = 0; j < PlayerFuncs.EnemiesKill.Length; j++)
-			{
-				if (PlayerFuncs.EnemiesKill[j] == null)
-				{
-					PlayerFuncs.EnemiesKill[j] = gameObject;
-					FindObjectOfType<AudioManager>().Play("Kill");
-					break;
-				}
-			}*/
-			//PlayerFuncs.EnemiesKill = gameObject;
-			//PlayerFuncs.SkillSetter += 0.5f;
-			//if (GameObject.FindObjectOfType<FillKnife>() != null)
-				//GameObject.FindObjectOfType<FillKnife>().StartCoroutine("FillButton");
-			//Debug.Log(PlayerFuncs.SkillSetter);
 		}
-		//Debug.Log(CheckifPlayerInfrontofEnemy(player));
-		//Debug.Log(PlayerFuncs.Invisible <= 0);
-		//Debug.Log(!PlayerFuncs.IsThereGate(gameObject.transform));
-		//Debug.Log(PlayerFuncs.LightOffTurns <= 0);
 		if (CheckifPlayerInfrontofEnemy(player) && PlayerFuncs.Invisible <= 0 && !PlayerFuncs.IsThereGate(gameObject.transform) && !PlayerFuncs.IsThereCamera(gameObject.transform) && PlayerFuncs.LightOffTurns <= 0)
 		{
-			//Application.LoadLevel(0);
-				//Debug.Log("LOLIKS");
 				gameObject.GetComponent<Animator>().SetBool("IsKilling", true);
-				//StopAllCoroutines();
 				PlayerFuncs.StartCoroutine("KillingAnimation", gameObject);
-				//ListOfMovingEnemies[i].transform.GetChild(1).gameObject.SetActive(true);
-				//Debug.Log(ListOfMovingEnemies[i].transform.GetChild(1));
-				//ListOfMovingEnemies[i].transform.GetChild(1).
-				//return null;
-				//this.enabled = false;
-				//Application.LoadLevel(0);
 		}
 		if (projection != null)
 		{
@@ -432,7 +194,6 @@ public class MotionlessEnemy : MonoBehaviour
 			&& transform.position.z == projection.transform.position.z
 			&& (!CheckIfFacing(projection) || PlayerFuncs.LightOffTurns >= 0))
 			{
-				//Destroy(gameObject);
 				gameObject.GetComponent<Animator>().SetBool("IsDead", true);
 				for (int j = 0; j < PlayerFuncs.EnemiesKill.Length; j++)
 				{
@@ -442,11 +203,6 @@ public class MotionlessEnemy : MonoBehaviour
 						break;
 					}
 				}
-				//PlayerFuncs.EnemiesKill = gameObject;
-				//PlayerFuncs.SkillSetter += 0.5f;
-			//	if (GameObject.FindObjectOfType<FillKnife>() != null)
-				//	GameObject.FindObjectOfType<FillKnife>().StartCoroutine("FillButton");
-				//Debug.Log(PlayerFuncs.SkillSetter);
 			}
 			if (CheckifPlayerInfrontofEnemy(projection) && !PlayerFuncs.IsThereGate(gameObject.transform) && !PlayerFuncs.IsThereCamera(gameObject.transform) && PlayerFuncs.LightOffTurns <= 0)
 			{
