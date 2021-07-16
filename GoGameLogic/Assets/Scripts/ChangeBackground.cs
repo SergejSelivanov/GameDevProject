1347ebd30b9c0bf426df760b997e168b3a6ba274
Festermo <79100655+Festermo@users.noreply.github.com>
2021-07-16 12:42:30 +0300

    Level selector design

---
+++ GoGameLogic/Assets/Scripts/ChangeBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeBackground : MonoBehaviour
+{
+    float Timer = 0;
+    int index = 0;
+    public Sprite[] Images;
+
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >= 10f)
+        {
+            Timer = 0;
+            index++;
+            if (index >= Images.Length)
+                index = 0;
+            gameObject.GetComponent<Image>().sprite = Images[index];
+        }
+    }
+}
