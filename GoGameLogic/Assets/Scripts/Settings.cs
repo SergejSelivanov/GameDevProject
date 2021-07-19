a0b9d62b06919704ed5a761f7a3a59d514430ddc
Festermo <79100655+Festermo@users.noreply.github.com>
2021-07-19 15:09:45 +0300

    quality settings

--- GoGameLogic/Assets/Scripts/Settings.cs
+++ GoGameLogic/Assets/Scripts/Settings.cs
@@ -26,6 +26,11 @@ public class Settings : MonoBehaviour
 
     }*/
 
+    private void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
     public void OpenSettings()
     {
         MenuPanel.SetActive(false);
@@ -62,6 +67,12 @@ public class Settings : MonoBehaviour
             gameObject.transform.Find("SoundsToggle").gameObject.GetComponent<Toggle>().isOn = true;
            // PlayerPrefs.SetInt("SoundsVolume", 1);
         }
+        if (QualitySettings.GetQualityLevel() == 0)
+            gameObject.transform.Find("GraphicsDropdown").gameObject.GetComponent<Dropdown>().value = 0;
+        else if (QualitySettings.GetQualityLevel() == 1)
+            gameObject.transform.Find("GraphicsDropdown").gameObject.GetComponent<Dropdown>().value = 1;
+        else if (QualitySettings.GetQualityLevel() == 2)
+            gameObject.transform.Find("GraphicsDropdown").gameObject.GetComponent<Dropdown>().value = 2;
         audioManager = FindObjectOfType<AudioManager>().gameObject;
         //DontDestroyOnLoad(gameObject);
     }
@@ -88,7 +99,20 @@ public class Settings : MonoBehaviour
         }
         if (gameObject.transform.Find("MusicToggle").gameObject.GetComponent<Toggle>().isOn == false && PlayerPrefs.GetInt("MusicVolume") == 1)
             PlayerPrefs.SetInt("MusicVolume", 0);
+        if (gameObject.transform.Find("GraphicsDropdown").gameObject.GetComponent<Dropdown>().value == 0 && QualitySettings.GetQualityLevel() != 0)
+        {
+           //PlayerPrefs.SetInt("GraphicsIndex", 0);
+            SetQuality(0);
+        }
+        if (gameObject.transform.Find("GraphicsDropdown").gameObject.GetComponent<Dropdown>().value == 1 && QualitySettings.GetQualityLevel() != 1)
+        {
+            //PlayerPrefs.SetInt("GraphicsIndex", 1);
+            SetQuality(1);
+        }
+        if (gameObject.transform.Find("GraphicsDropdown").gameObject.GetComponent<Dropdown>().value == 2 && QualitySettings.GetQualityLevel() != 2)
+        {
+            //PlayerPrefs.SetInt("GraphicsIndex", 2);
+            SetQuality(2);
+        }
     }
-
-
 }
