7a4ee9f3efabfca2c727bffd7244cd508ebfcd30
Festermo <79100655+Festermo@users.noreply.github.com>
2021-07-21 17:50:12 +0300

    Two ready cutscenes

--- GoGameLogic/Assets/Scripts/FirstCutscene.cs
+++ GoGameLogic/Assets/Scripts/FirstCutscene.cs
@@ -16,6 +16,7 @@ public class FirstCutscene : MonoBehaviour
 
     void Start()
     {
+        FindObjectOfType<AudioManager>().Play("Siren");
         StartCoroutine(StartDialog());
     }
 
---
+++ GoGameLogic/Assets/Scripts/SecondCutscene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondCutscene : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject npc;
+    public GameObject light;
+    bool IsStarted = false;
+
+    IEnumerator StartCutscene()
+    {
+        npc.GetComponent<Animator>().SetTrigger("IsReady");
+        FindObjectOfType<AudioManager>().Play("Running");
+        yield return new WaitForSeconds(0.2f);
+        player.GetComponent<Animator>().applyRootMotion = true;
+        player.GetComponent<Animator>().SetTrigger("IsReady");
+        yield return new WaitForSeconds(2.9f);
+        FindObjectOfType<AudioManager>().Play("Breaking");
+        light.SetActive(true);
+        yield return new WaitForSeconds(1.8f);
+        FindObjectOfType<LevelLoader>().LoadNextLevel();
+        yield return null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.Find("Dialogue").gameObject.activeSelf == false && IsStarted == false)
+        {
+            IsStarted = true;
+            StartCoroutine(StartCutscene());
+            //npc.GetComponent<Animator>().SetTrigger("IsReady");
+            //Debug.Log("a");
+        }
+
+    }
+}
