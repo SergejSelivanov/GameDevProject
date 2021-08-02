ebfe235b3fd7c339ce2461f32623998030892286
Festermo <79100655+Festermo@users.noreply.github.com>
2021-08-02 21:54:14 +0300

    final cutscene done

--- GoGameLogic/Assets/Scripts/FinalCutscene.cs
+++ GoGameLogic/Assets/Scripts/FinalCutscene.cs
@@ -15,25 +15,31 @@ public class FinalCutscene : MonoBehaviour
     public GameObject dialog;
     public GameObject car;
     public GameObject statueLight;
+    private AudioManager audioManager;
     private bool DialogHasStarted;
 
     IEnumerator MoveSecond()
     {
+        audioManager.Play("Stun");
         yield return new WaitForSeconds(1f);
         statueLight.SetActive(true);
+        //audioManager.Play("Stun");
+        //audioManager.Play("Stun1");
         yield return new WaitForSeconds(2f);
         statueLight.SetActive(false);
         yield return new WaitForSeconds(1f);
         statueLight.SetActive(true);
-        yield return new WaitForSeconds(1.2f);
+       // audioManager.Play("Stun2");
+        yield return new WaitForSeconds(1.5f);
         statueLight.SetActive(false);
-        yield return new WaitForSeconds(1.8f);
+        yield return new WaitForSeconds(1.5f);
 
 
         //yield return new WaitForSeconds(7f);
         sixthCamera.SetActive(false);
         seventhCamera.SetActive(true);
         car.GetComponent<Animator>().SetTrigger("IsFlying");
+        audioManager.Play("Car");
         yield return new WaitForSeconds(6f);
         SceneManager.LoadScene(0);
     }
@@ -41,6 +47,7 @@ public class FinalCutscene : MonoBehaviour
 
     IEnumerator MoveFirstCamera()
     {
+        audioManager.Play("City");
         float StartFOV = firstCamera.GetComponent<Camera>().fieldOfView;
         Quaternion StartRotation = firstCamera.transform.rotation;
         Vector3 StartPosition = firstCamera.transform.position;
@@ -58,6 +65,16 @@ public class FinalCutscene : MonoBehaviour
             firstCamera.transform.position += PositionDiff;
             yield return new WaitForSeconds(0.01f);
         }
+        audioManager.Play("Room");
+        /*for (float i = 0; i < 1; i += 0.01f)//changing FOV, rotation and position of camera
+        {
+            firstCamera.GetComponent<Camera>().fieldOfView += FovDiff; //changing FOV
+            firstCamera.transform.rotation = Quaternion.Euler(firstCamera.transform.rotation.eulerAngles.x - RotationDiffX, firstCamera.transform.rotation.eulerAngles.y - RotationDiffY, 0);
+            firstCamera.transform.position += PositionDiff;
+            yield return new WaitForSeconds(0.01f);
+        }*/
+
+
         firstCamera.SetActive(false);
         thirdCamera.SetActive(true);
         //yield return new WaitForSeconds(7.5f);
@@ -87,6 +104,7 @@ public class FinalCutscene : MonoBehaviour
 
     void Start()
     {
+        audioManager = FindObjectOfType<AudioManager>();
         StartCoroutine(MoveFirstCamera());
     }
 
