beb2bedc4e22778c2f6f60bb46adbe95b42c7692
Festermo <79100655+Festermo@users.noreply.github.com>
2021-08-04 00:25:47 +0300

    fixed arrows at all levels

---
+++ GoGameLogic/Assets/Scripts/RotateArrowsLevel10.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateArrowsLevel10 : MonoBehaviour
+{
+    public GameObject arrows;
+    private void OnTriggerEnter(Collider other)
+    {
+        arrows.transform.rotation = Quaternion.Euler(0, 0, -90);
+    }
+}
