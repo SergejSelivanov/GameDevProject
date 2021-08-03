46f8d6a1d8de60dfd22f7c02009f5f250a16cedf
Festermo <79100655+Festermo@users.noreply.github.com>
2021-08-04 00:53:27 +0300

    level 5 dialog

---
+++ GoGameLogic/Assets/Scripts/DialogLvl5.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogLvl5 : MonoBehaviour
+{
+    public GameObject Dialog;
+    private SkillPickUp pickup;
+
+    private void OpenDialog()
+    {
+        Dialog.SetActive(true);
+    }
+
+    private void FixedUpdate()
+    {
+        pickup = FindObjectOfType<SkillPickUp>();
+        if (pickup == null)
+        {
+            OpenDialog();
+            Destroy(this);
+        }
+    }
+}
