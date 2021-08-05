a445f01b4a3648def1691b7e17549e3ca7171b58
Festermo <79100655+Festermo@users.noreply.github.com>
2021-08-05 16:19:53 +0300

    level 10 fix rotating of arrows

--- GoGameLogic/Assets/Scripts/RotateArrowsLevel10.cs
+++ GoGameLogic/Assets/Scripts/RotateArrowsLevel10.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class RotateArrowsLevel10 : MonoBehaviour
 {
     public GameObject arrows;
+    private GameObject player;
+
     private void OnTriggerEnter(Collider other)
     {
-        arrows.transform.rotation = Quaternion.Euler(0, 0, -90);
+        if (player.transform.rotation.eulerAngles.y == gameObject.transform.rotation.eulerAngles.y
+        || Mathf.Abs(player.transform.rotation.eulerAngles.y - gameObject.transform.rotation.eulerAngles.y) < 0.1f)
+            arrows.transform.rotation = Quaternion.Euler(0, 0, -90);
+        else
+            arrows.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 }
