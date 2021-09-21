d8d965b4983d778ce7655465f16aecf3e9c4eb5e
Festermo <79100655+Festermo@users.noreply.github.com>
2021-09-21 15:55:25 +0300

    new prompts

---
+++ GoGameLogic/Assets/Scripts/prompt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prompt : MonoBehaviour
+{
+    public GameObject dialog;
+
+    public void ShowPrompt()
+    {
+        dialog.SetActive(true);
+    }
+}
